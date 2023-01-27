39f1910b2d8efaa28d4be822d7e2f0b75b71258a
aurelo <zlatko.gudasic@gmail.com>
2023-01-27 07:28:45 +0100

    google and github social login

--- src/main/java/com/kanezi/authdb/security/SecurityConfig.java
+++ src/main/java/com/kanezi/authdb/security/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.kanezi.authdb.security;
 
 import lombok.extern.log4j.Log4j2;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.*;
 
 @Configuration
 @Log4j2
@@ -23,6 +28,7 @@ public class SecurityConfig {
                 //                .formLogin(Customizer.withDefaults())
                 .formLogin(f -> f.loginPage("/login")
                                  .usernameParameter("email"))
+                .oauth2Login(oc -> oc.loginPage("/login"))
                 .logout(l -> l.clearAuthentication(true)
                               .invalidateHttpSession(true)
                               .logoutSuccessUrl("/"))
@@ -69,5 +75,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    ApplicationListener<AuthenticationSuccessEvent> logAuthenticationSuccessEvent() {
+        return event -> log.info("User logged in: {}", event.getAuthentication());
     }
 }
